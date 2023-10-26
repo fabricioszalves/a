618b5e9c0859a64b354295d44f95b88f4635d0c8
Fabricio Alves <fabricioszalves@gmial.com>
2023-10-26 13:29:19 -0300

    1/4 do projeto

---
+++ src/Funcionario.java
@@ -0,0 +1,18 @@
+public class Funcionario {
+    private String nome;
+    private int idade;
+    private int idf;
+    private String endereco;
+    public String getEndereco() {
+        return endereco;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public int getIdf(){
+        return idf;
+    }
+}
